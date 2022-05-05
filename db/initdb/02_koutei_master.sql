69c4aaeab28b0193a6d9c5fd073b12ef41e72d96
EC2 Default User <ec2-user@ip-172-31-2-220.ap-northeast-1.compute.internal>
2022-05-05 09:50:49 +0000

    DBinit追加（権限設定うまくいってない）

---
+++ db/initdb/02_koutei_master.sql
@@ -0,0 +1,4 @@
+CREATE TABLE koutei_master (
+  id integer, 
+  name varchar(30)
+);
---
+++ db/initdb/10_insert_koutei_master.sql
@@ -0,0 +1,7 @@
+INSERT INTO koutei_master
+    (id, name)
+VALUES
+    (1, 'koutei1'),
+    (2, 'koutei2'),
+    (3, 'koutei3')
+;
